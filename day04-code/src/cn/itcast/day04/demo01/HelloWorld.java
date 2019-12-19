116cc3f20a7129f22134b53c8b9172b5eca535f3
liyaodong <yaodongli0810@gmail.com>
2019-12-19 11:05:44 +0800

    首次提交到本地代码库 测试HelloWorld!

--- day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
+++ day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -3,5 +3,7 @@ package cn.itcast.day04.demo01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        /*System.out.println("Hello World!");*/
+//      System.out.println("Hello World!");  //快捷键联系使用
     }
 }
